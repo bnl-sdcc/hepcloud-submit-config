e094e24a3cd229cd6c8203d038f91b0f6c964e0b
jhover <jhover@starkhorn.rhic.bnl.gov>
2019-06-05 15:44:15 -0400

    Update.

--- hepcloud/mkprojectnamemapfile.py
+++ hepcloud/mkprojectnamemapfile.py
@@ -1,29 +1,103 @@
 #!/bin/env python
 #
+# Looks up members of groups, and individual users in mkprojectnamemapfile.conf
+# Generates condor usermap file in form:
+#    * <username>  <ProjectName>
+#    * <username2> <ProjectName>
+#
+# based on target it config file. 
+#
+#
 # 
 
+import argparse
 from configparser import ConfigParser
 import logging
 import grp
 import pwd
 
-def create_mapfile(config, filepath):
-    logging.debug("Using config: %s   writing to: %s" % (config, filepath))
+class MapfileHandler(object):
     
-    sect = config.get('groupmappings')
+    def __init__(self, config):
+        logging.debug("Using config: %s   " % (config))
+        self.config = config
+        self.defaulttarget = config.get('main', 'defaulttarget')
+        self.header = config.get('main','header')        
+        
+        
+    def create_mapfile(self):
+        filelines = []  # list of strings
+        
+        filelines.append(self.header)
+            
+        goptions = self.config.options('groupmappings')
+        for go in goptions:
+            target=self.config.get('groupmappings',go)
+            logging.debug("group is %s target is %s" % (go, target))
+            db = grp.getgrnam(go)
+            for u in db.gr_mem:
+                line = "* %s %s" % (u, target )
+                filelines.append(line)
+                
+        uoptions = self.config.options('usermappings')
+        for uo in uoptions:
+            logging.debug("user is %s" % uo)
+            target = self.config.get('usermappings', uo)
+            line = "* %s %s" % (uo, target  )
+            logging.debug("Line is %s" % line)
+            filelines.append(line)
+        
+        dt = self.config.get('main','defaulttarget')
+        line = "* /.*/ %s" % dt
+        filelines.append(line)
+        
+        self.map = ""
+        for line in filelines:
+            self.map += "%s\n" % line
+        logging.debug("Successfully build map...")
+            
 
+    def write_mapfile(self):
+        filepath = self.config.get('main','mapfile')
+        logging.debug("Writing map to %s" % filepath)
+        f = open(filepath, 'w')
+        f.write(self.map)
+        
 
 
 if __name__ == '__main__':
-    CONFFILE = "/etc/condor/mkprojectnamemapfile.conf"
-    MAPFILE = "/etc/condor/projectname.usermap"
 
     logging.basicConfig(format='%(asctime)s (UTC) [ %(levelname)s ] %(name)s %(filename)s:%(lineno)d %(funcName)s(): %(message)s')
-    logging.getLogger().setLevel(logging.DEBUG)
     
-    cp = ConfigParser()
-    cp.read(CONFFILE)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--conf', 
+                        action="store", 
+                        dest='conffile', 
+                        default="/etc/condor/mkprojectnamemapfile.conf",
+                        help='configuration file.')
+
+    parser.add_argument('-m', '--mapfile', 
+                        action="store", 
+                        dest='mapfile', 
+                        default="/etc/globus/globus-acct-map",
+                        help='path to write map file')    
+    
+    parser.add_argument('-d', '--debug', 
+                        action="store_true", 
+                        dest='debug', 
+                        help='debug logging')
+    args= parser.parse_args()
+    
+    if args.debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+    
+    config = ConfigParser()
+    config.read(args.conffile)
      
-    create_mapfile(cp, MAPFILE)
+    mh = MapfileHandler(config)
+    mh.create_mapfile()
+    print(mh.map)
+    mh.write_mapfile()
+    
 
     
